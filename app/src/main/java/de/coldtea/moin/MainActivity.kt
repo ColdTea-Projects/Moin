0071dd72724dd70a4c45e626911e1adceb9a41cd
Yasar Naci Gündüz <yasarnacigunduz@gmail.com>
2021-06-09 21:15:16 +0200

    project structure reset

--- app/src/main/java/de/coldtea/moin/MainActivity.kt
+++ app/src/main/java/de/coldtea/moin/MainActivity.kt
@@ -13,17 +13,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.moin_activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_alarms, R.id.navigation_playlists
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
