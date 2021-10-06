f6138f7dde58a464e040fba8970fefe236bc20d1
Yasar Naci Gündüz <yasarnacigunduz@gmail.com>
2021-10-06 22:05:23 +0200

    Added clear text input

--- app/src/main/java/de/coldtea/moin/ui/searchspotify/SearchSpotifyActivity.kt
+++ app/src/main/java/de/coldtea/moin/ui/searchspotify/SearchSpotifyActivity.kt
@@ -8,6 +8,7 @@ import android.text.TextUtils
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
@@ -15,9 +16,11 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import de.coldtea.moin.R
+import de.coldtea.moin.data.network.spotify.model.TokenResponse
 import de.coldtea.moin.databinding.ActivitySearchSpotifyBinding
 import de.coldtea.moin.domain.model.extensions.getSearchResultBundle
 import de.coldtea.moin.domain.model.playlist.PlaylistName
+import de.coldtea.moin.domain.model.spotify.SearchResult
 import de.coldtea.moin.domain.services.SpotifyService
 import de.coldtea.moin.extensions.convertToAuthorizationResponse
 import de.coldtea.moin.services.model.*
@@ -26,8 +29,10 @@ import de.coldtea.moin.ui.searchspotify.adapter.SpotifySearchAdapter
 import de.coldtea.moin.ui.searchspotify.adapter.model.SpotifySearchResultBundle
 import de.coldtea.moin.ui.searchspotify.adapter.model.SpotifySearchResultDelegateItem
 import de.coldtea.moin.ui.services.DialogManager
+import kotlinx.android.synthetic.main.activity_search_spotify.*
 import kotlinx.coroutines.flow.collect
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 class SearchSpotifyActivity : AppCompatActivity() {
 
@@ -105,37 +110,57 @@ class SearchSpotifyActivity : AppCompatActivity() {
                 count > 0 && !viewModel.refreshTokenExist -> startActivity(viewModel.getAuthorizationIntent())
                 count == 0 -> clearPlaylist()
             }
+
+            binding?.setClearButtonVisibility(count > 0)
+        }
+
+        this?.clearText?.setOnClickListener {
+            this.searchInput.text?.clear()
         }
     }
 
     private fun initSpotify() = lifecycleScope.launchWhenResumed {
         viewModel.spotifyState.collect {
             when (it) {
-                ConnectionSuccess -> {
-                    binding?.searchInput?.isEnabled = true
-                }
-                ConnectionFailed -> {
-                    alertDialog = DialogManager.buildDialog(
-                        this@SearchSpotifyActivity,
-                        getString(R.string.spotify_connection_failed),
-                        getString(R.string.ok),
-                        { alertDialog?.dismiss() }
-                    )
-
-                    alertDialog?.show()
-                }
-                is AccessTokenReceived -> {
-                    val keyword = binding?.searchInput?.text.toString()
-                    if (keyword.isNotEmpty() && it.tokenResponse?.accessToken != null) {
-                        viewModel.search(it.tokenResponse.accessToken, keyword)
-                    }
-                }
-                is SearchResultReceived -> {
-                    searchResultAdapter.items = it.searchResult
-                        ?.getSearchResultBundle(::onPlayClicked, ::onItemClicked)
-                }
+                ConnectionSuccess -> onConnectionSuccess()
+                ConnectionFailed -> onConnectionFailed()
+                is AccessTokenReceived -> onActivationTokenReceived(it.tokenResponse)
+                is SearchResultReceived -> onSearchResultReceived(it.searchResult)
+                else -> Timber.w("Received illegal spotify state")
             }
         }
+    }
+
+    private fun onConnectionSuccess(){
+        binding?.searchInput?.isEnabled = true
+        binding?.searchInput?.requestFocus()
+        openKeyboard()
+    }
+
+    private fun onConnectionFailed(){
+        alertDialog = DialogManager.buildDialog(
+            this@SearchSpotifyActivity,
+            getString(R.string.spotify_connection_failed),
+            getString(R.string.ok),
+            {
+                alertDialog?.dismiss()
+                finish()
+            }
+        )
+
+        alertDialog?.show()
+    }
+
+    private fun onActivationTokenReceived(tokenResponse: TokenResponse?) {
+        val keyword = binding?.searchInput?.text.toString()
+        if (keyword.isNotEmpty() && tokenResponse?.accessToken != null) {
+            viewModel.search(tokenResponse.accessToken, keyword)
+        }
+    }
+
+    private fun onSearchResultReceived(searchResult: SearchResult?){
+        searchResultAdapter.items = searchResult
+            ?.getSearchResultBundle(::onPlayClicked, ::onItemClicked)
     }
 
     private fun onPlayClicked(id: String) {
@@ -188,8 +213,21 @@ class SearchSpotifyActivity : AppCompatActivity() {
         inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
     }
 
+    private fun openKeyboard(){
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(currentFocus, 0)
+    }
+
     private fun clearPlaylist(){
         searchResultAdapter.items = listOf()
+    }
+
+    private fun ActivitySearchSpotifyBinding.setClearButtonVisibility(isVisible: Boolean) = if(isVisible){
+        searchInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0,0)
+    }else{
+        searchInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_search_24,0)
+    }.also {
+        clearText.isVisible = isVisible
     }
 
     inner class SearchResultScrollListener : RecyclerView.OnScrollListener(){
