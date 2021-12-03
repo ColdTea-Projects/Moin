270d3c6d044856e37b32fdfc5fe88e98703ca6c6
Yasar Naci Gündüz <yasarnacigunduz@gmail.com>
2021-12-03 23:54:17 +0100

    Provided null safe responses in Weather repository

--- app/src/main/java/de/coldtea/moin/data/WeatherRepository.kt
+++ app/src/main/java/de/coldtea/moin/data/WeatherRepository.kt
@@ -9,6 +9,8 @@ import de.coldtea.moin.domain.model.extensions.toHourlyForecast
 import de.coldtea.moin.domain.model.forecast.HourlyForecast
 import de.coldtea.moin.extensions.convertToEntitylist
 import de.coldtea.moin.extensions.getTopOfTheHour
+import retrofit2.HttpException
+import timber.log.Timber
 
 class WeatherRepository(
     private val weatherForecastApi: WeatherForecastApi,
@@ -22,18 +24,30 @@ class WeatherRepository(
         forecast?.updateForecastsDatabase(cityName)
     }
 
-    suspend fun getHourlyForecast() = moinDatabase
-        .daoHourlyForecast
-        .getHourlyForecasts()
-        .map {
-            it.toHourlyForecast()
+    suspend fun getHourlyForecast() =
+        try {
+            moinDatabase
+                .daoHourlyForecast
+                .getHourlyForecasts()
+                .map {
+                    it.toHourlyForecast()
+                }
+        } catch (e: HttpException) {
+            Timber.e("Moin --> getHourlyForecast: $e")
+            null
         }
 
-    suspend fun getHourlyForecastByCity(cityName: String) = moinDatabase
-        .daoHourlyForecast
-        .getHourlyForecastsByCity(cityName)
-        .map {
-            it.toHourlyForecast()
+    suspend fun getHourlyForecastByCity(cityName: String) =
+        try {
+            moinDatabase
+                .daoHourlyForecast
+                .getHourlyForecastsByCity(cityName)
+                .map {
+                    it.toHourlyForecast()
+                }
+        } catch (e: HttpException) {
+            Timber.e("Moin --> getHourlyForecastByCity: $e")
+            null
         }
 
     private suspend fun WeatherResponse.updateForecastsDatabase(cityName: String) {
@@ -46,19 +60,30 @@ class WeatherRepository(
     }
 
     suspend fun getForecast(): HourlyForecast? =
-        moinDatabase
-            .daoHourlyForecast
-            .getForecastsAt(getTopOfTheHour())
-            ?.toHourlyForecast()
+        try {
+            moinDatabase
+                .daoHourlyForecast
+                .getForecastsAt(getTopOfTheHour())
+                ?.toHourlyForecast()
+        } catch (e: HttpException) {
+            Timber.e("Moin --> getForecast: $e")
+            null
+        }
 
-    private suspend fun getWeatherForThreeDays(cityName: String) =
-        weatherForecastApi.getForecast(cityName, 3)
+    private suspend fun getWeatherForThreeDays(cityName: String): WeatherResponse? =
+        try {
+            weatherForecastApi.getForecast(cityName, 3)
+        } catch (e: HttpException) {
+            Timber.e("Moin --> getWeatherForThreeDays: $e")
+            null
+        }
 
     suspend fun getCurrentByLatLong(latLong: LatLong?) =
         latLong?.let { weatherForecastApi.getCurrent("${latLong.lat},${latLong.long}") }
 
     suspend fun isUpdateNeeded(cityName: String): Boolean {
-        val forecastBoundaryObject = getHourlyForecastByCity(cityName).toForecastBoundary()
+        val hourlyForecastByCity = getHourlyForecastByCity(cityName)?:return false
+        val forecastBoundaryObject = hourlyForecastByCity.toForecastBoundary()
 
         if (forecastBoundaryObject.noForecastData()
             || forecastBoundaryObject.dataSizeNotBigEnough()
