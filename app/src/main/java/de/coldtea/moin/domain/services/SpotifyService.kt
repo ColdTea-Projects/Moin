501699fc7b54a0fa6c05075ef83b6b080a7a4595
Yasar Naci Gündüz <yasarnacigunduz@gmail.com>
2022-01-12 22:54:34 +0100

    Hid the regenerated api keys

--- app/src/main/java/de/coldtea/moin/domain/services/SpotifyService.kt
+++ app/src/main/java/de/coldtea/moin/domain/services/SpotifyService.kt
@@ -1,7 +1,7 @@
 package de.coldtea.moin.domain.services
 
 object SpotifyService{
-    const val CLIENT_ID = "4fd1cb8351e940cf898e79ba552d6d77"
+    const val CLIENT_ID = "SPOTIFY_CLIENT_ID"
     const val REDIRECT_URI_ROOT = "de.coldtea.moin"
     const val REDIRECT_URI = "$REDIRECT_URI_ROOT:/callback"
 }
